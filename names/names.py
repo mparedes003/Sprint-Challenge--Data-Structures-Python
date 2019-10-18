282f90da7908d0fba474cf92f992a64f4ec3ce4c
mparedes003 <onnabugeishadev@gmail.com>
2019-10-18 08:33:14 -0700

    names.py complete. Try to optimize even more later.

--- names/names.py
+++ names/names.py
@@ -9,14 +9,19 @@ f.close()
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
-
-duplicates = []
+# original code runs in 10.983 secs
+'''duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
-            duplicates.append(name_1)
+            duplicates.append(name_1)'''
+
+# First Pass at optimization
+# use a list comprehension runs in 3.902
+duplicates = [
+    name_2 for name_1 in names_1 for name_2 in names_2 if name_1 == name_2]
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
